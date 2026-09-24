974f1ce100429e3ad690356c1a79cb034ff56052
artsurdulli <artsurdulli21@gmail.com>
2026-09-24 21:19:45 +0200

    Update dependencies and build config for native modules

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,19 @@
 module.exports = {
   preset: 'react-native',
+  // The RN preset's default only allows-list RN's own packages; navigation,
+  // redux and their peers ship ESM builds in node_modules that also need
+  // to go through babel-jest, or `import` statements blow up under Jest.
+  transformIgnorePatterns: [
+    'node_modules/(?!(' +
+      '(jest-)?react-native' +
+      '|@react-native(-community)?' +
+      '|@react-native-async-storage' +
+      '|@react-navigation' +
+      '|react-redux' +
+      '|@reduxjs/toolkit' +
+      '|immer' +
+      '|react-native-screens' +
+      '|react-native-safe-area-context' +
+      ')/)',
+  ],
 };
